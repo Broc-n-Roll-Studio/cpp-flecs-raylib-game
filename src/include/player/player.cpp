e003a12dcfeaf18041045fe6d6239f08ce076983
gvrDev <guilhermevrodrigues123@gmail.com>
2023-09-30 20:52:45 -0300

    ref: code ref

--- src/include/player/player.cpp
+++ src/include/player/player.cpp
@@ -1,8 +1,13 @@
 #include "entity/entity.h"
 #include "player.h"
 
+using broc::entity::Drawable;
+using broc::entity::Movable;
+using broc::player::Player;
+
 void broc::player::setup(flecs::world& world) {
-  auto e = world.entity().set([](broc::entity::Movable& m, broc::entity::Drawable& d) {
+  world.component<Player>();
+  auto e = world.entity().add<Player>().set([](Movable& m, Drawable& d) {
     m = {{800.0 / 2, 450.0 / 2, 0}, {1, 0, 0}, 1};
     d = {50, RED};
   });
--- src/include/player/player.h
+++ src/include/player/player.h
@@ -3,6 +3,7 @@
 
 namespace broc {
   namespace player {
+    struct Player {};
     void setup(flecs::world& world);
-  }
+  }  // namespace player
 }  // namespace broc
