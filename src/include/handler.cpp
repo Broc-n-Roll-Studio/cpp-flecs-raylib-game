059ddec7007c5524d6ccead0574ef302bfe9e3fd
gvrDev <guilhermevrodrigues123@gmail.com>
2023-10-02 04:04:57 -0300

    fix: removed debug

--- src/include/handler.cpp
+++ src/include/handler.cpp
@@ -1,8 +1,6 @@
 #include "handler.h"
-#include <iostream>
 
 void Context::process_contexts(flecs::world &world, std::vector<Context> &contexts) {
-  std::cout << contexts.size() << std::endl;
   for (auto context : contexts) {
     context.setup_components(world);
   }
